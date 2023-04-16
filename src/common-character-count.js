375f47cc53eb32b39fda1b066bb35a8805204ac8
Artem Pukhov <art.puhov@gmail.com>
2023-04-16 15:29:57 +0300

    Refactor code base

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,10 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(s1, s2 ) {
-  s1+s2)
-}
+function getCommonCharacterCount(s1, s2) {
+  // s1 = "aabcc"
+  // s2 = "adcaa"
+  arr1 = Array.from(s1);
+  arr2 = Array.from(s2);
 
+//   let count = 0;
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++)
+
+//       if (arr1[i] === arr2[j]) {
+//         count += 1;
+//         alert('count = ' + count);
+//         break;
+
+//       }
+//   }
+//   return count;
+// }
+let count = 0;
+for (let i = 0; i < arr1.length; i++) {
+  if (s2.includes(i))
+count++, s2 = s2.remove(i);
+  
+ }
+ return count
+}
 module.exports = {
   getCommonCharacterCount
 };
